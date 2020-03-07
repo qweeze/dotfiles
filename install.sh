e53c13e9f3eef573f41db8f9c884ce17f6327d08
qweeze <qweeeze@gmail.com>
2020-03-08 01:36:32 +0500

    install.sh shellcheck fixes

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ declare -a LINKS=(
   "tmux.conf"
 )
 
-DOTFILES_PATH=$(dirname $(realpath $0))
+DOTFILES_PATH=$(dirname "$(realpath "$0")")
 
-for link in ${LINKS[@]}; do
-  if [ -L ~/\.$link ]; then
+for link in "${LINKS[@]}"; do
+  if [ -L "$HOME/.$link" ]; then
     echo ".$link exists, skipping"
   else
-    ln -s $DOTFILES_PATH/$link ~/.$link
+    ln -s "$DOTFILES_PATH"/"$link" ~/."$link"
     echo "Linked created ~/.$link -> $DOTFILES_PATH/$link"
   fi
 done
